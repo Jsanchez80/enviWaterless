ee1b5ad0991445ca69e965d5223d7276a6a32b9f
Jose G. Sanchez <j_gsanchez80@hotmail.com>
2016-11-29 11:48:11 -0600

    Update:

--- env.template.php
+++ env.template.php
@@ -2,10 +2,10 @@
 
 return array(
 
-    'DB_HOST' => '127.0.0.1',
-    'DB_NAME' => 'adlister_db',
-    'DB_USER' => 'vagrant',
-    'DB_PASS' => 'vagrant',
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASS' => '',
 
-    'USER_PASS' => 'vagrant',
+    'USER_PASS' => '',
 );
